4fed13f71e3508a90b0737129f0c38f799fb42d4
Mitchell <Hmitchell.hall@gmail.com>
2021-10-12 15:48:56 -0500

    added text to home component

--- harry-potter-app/src/Components/Home.js
+++ harry-potter-app/src/Components/Home.js
@@ -1,7 +1,8 @@
 function Home(){
     return(
         <div className="homePage">
-            <h1>Home</h1>
+            <h1>Harry Potter App</h1>
+            <h4>Click "Characters" to search all the characters in Harry Potter, and add them to your favorites list to see more details about them</h4>
         </div>
     )
 }
